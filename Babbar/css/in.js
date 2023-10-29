07029870fe8e6d39af62d332dce1344a40de07d8
Harshal Shinde <shindeharshal009@gmail.com>
2023-10-30 00:42:36 +0530

    css projects

---
+++ Babbar/css/in.js
@@ -0,0 +1,6 @@
+function consoleLog(val) {
+    console.log(val)
+    return val
+}
+
+consoleLog();
---
+++ js/play.js
@@ -0,0 +1,11 @@
+var largestNumber = function(nums) {
+    nums.reverse();
+    let arr = nums.join('');
+    return arr;
+
+};
+
+let nums = [10,2];
+
+let res = largestNumber(nums);
+console.log(res);
